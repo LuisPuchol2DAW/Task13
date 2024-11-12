94ea70e63c37aee723fd48907d3a9dda2ad0fe56
LuisPuchol2DAW <lpucholdelgado@cifpfbmoll.eu>
2024-11-12 19:28:08 +0100

    added buttons

--- app/src/main/java/com/example/task13_scrollingapp/MainActivity.java
+++ app/src/main/java/com/example/task13_scrollingapp/MainActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +21,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     private static final String TAG = "MainActivity";
     private GestureDetector gestureDetector;
     private float startX, startY, endX, endY;
+    private Button commentButton, edditButton, addButton;
+    private LinearLayout articleContainer;
+    private EditText commentTextView, editedArticleTextView;
+    private Boolean isEditing = false;
+    private TextView articleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +34,79 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         startStuff();
         startLayout();
         startGesture();
+        setListeners();
+    }
+
+    public void setListeners() {
+        commentListener();
+        editTextListener();
+    }
+
+    private void editTextListener() {
+        edditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEditing) {
+                    articleText.setText(editedArticleTextView.getText().toString());
+                    articleText.setVisibility(View.VISIBLE);
+                    editedArticleTextView.setVisibility(View.GONE);
+                    edditButton.setText("Edit");
+                } else {
+                    editedArticleTextView.setText(articleText.getText().toString());
+                    editedArticleTextView.setVisibility(View.VISIBLE);
+                    articleText.setVisibility(View.GONE);
+                    edditButton.setText("Save");
+                }
+                isEditing = !isEditing;
+            }
+        });
+    }
+
+    private void commentListener() {
+        commentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String commentText = commentTextView.getText().toString().trim();
+
+                if (commentText.isEmpty()) {
+                    return;
+                }
+
+                TextView commentView = createCommentTextView(commentText);
+
+                addTextView(commentView);
+
+                //reset comment text
+                commentTextView.setText("");
+
+            }
+        });
+    }
+
+    private void addTextView(TextView commentView) {
+        articleContainer.addView(commentView, articleContainer.getChildCount() - 1);
+    }
+
+    private @NonNull TextView createCommentTextView(String commentText) {
+        TextView commentView = new TextView(MainActivity.this);
+        commentView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        commentView.setText(commentText);
+        commentView.setPadding(8, 8, 8, 8);
+        return commentView;
     }
 
     public void startLayout() {
-        //button_toast = findViewById(R.id.button_toast);
+        articleContainer = findViewById(R.id.article_container);
+
+        commentButton = findViewById(R.id.button_comment);
+        commentTextView = findViewById(R.id.comment);
+
+        edditButton = findViewById(R.id.button_edit);
+        editedArticleTextView = findViewById(R.id.article_edit);
+        articleText = findViewById(R.id.article);
     }
 
     public void startStuff() {
